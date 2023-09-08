1167aa5dbc21b462633986b7d4accac010a99588
HenchaDev <henchaedits01@gmail.com>
2023-09-08 19:05:04 +0300

    Third commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from item.models import Item
 # Create your views here.
