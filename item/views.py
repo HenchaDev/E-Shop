23ea76d2402c14f6ad0fc37e103fd0b4bcea1493
HenchaDev <henchaedits01@gmail.com>
2023-09-08 18:16:19 +0300

    Third commit

--- item/views.py
+++ item/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import NewItemForm
 from .models import Item
 
 # Create your views here.
@@ -9,4 +12,23 @@ def detail(request, pk):
     return render(request, 'item/detail.html',{
         'item': item,
         'related_items': related_items,
+    })
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+       form = NewItemForm()
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'New Item'
     })
