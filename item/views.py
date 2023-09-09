cd03b7eabfe674ee7fcc7117f314bb4aba52f540
HenchaDev <henchaedits01@gmail.com>
2023-09-09 16:23:29 +0300

    Third commit

--- item/views.py
+++ item/views.py
@@ -7,8 +7,12 @@ from .models import Item, Category
 
 def items(request):
     query = request.GET.get('query', '')
-    categories = Category.object.all()
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
     items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
 
     if query:
         items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
@@ -17,6 +21,7 @@ def items(request):
         'items': items,
         'query': query,
         'categories': categories,
+        'category_id': int(category_id),
     })
 
 def detail(request, pk):
